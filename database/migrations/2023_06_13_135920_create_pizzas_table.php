608c220dd2643cfb61cafc5f4125cf61d8fe50a4
Francesca-23 <francescadidomenico4@gmail.com>
2023-06-13 17:06:19 +0200

    creazione migration

--- database/migrations/2023_06_13_135920_create_pizzas_table.php
+++ database/migrations/2023_06_13_135920_create_pizzas_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
+            $table->boolean('lievitazione_naturale')->default(true); //naturale - lievito
+            $table->boolean('gluten_free')->default(false); 
+            $table->string('base_pizza', 25); //focaccia - bianca - rossa
+            $table->tinyInteger('tipologia_pizza'); //romana - napoletana - genovese
+            $table->string('taglia_pizza', 15)->default('classic');
+            $table->string('nome_pizza', 25);
+            $table->string('ingredienti_principali');
+            $table->string('ingredienti_secondari')->nullable();
+            $table->decimal('prezzo', 4, 2);
             $table->timestamps();
         });
     }
